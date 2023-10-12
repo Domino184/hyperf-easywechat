a9514c19e52ce0a08931bb19f41a2bf5bb709c9e
domino <m18434900825@163.com>
2023-10-12 15:54:07 +0800

    edit

---
+++ src/EasyWechat.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Domino\HyperfEasyWechat;
+
+use Hyperf\Context\ApplicationContext;
+
+/**
+ * Class EasyWechat.
+ *
+ * @method static \EasyWeChat\OfficialAccount\Application  officialAccount(string $name = "default", array $config = [])
+ * @method static \EasyWeChat\Work\Application  work(string $name = "default", array $config = [])
+ * @method static \EasyWeChat\MiniProgram\Application  miniProgram(string $name = "default", array $config = [])
+ * @method static \EasyWeChat\Payment\Application  payment(string $name = "default", array $config = [])
+ * @method static \EasyWeChat\OpenWork\Application  openWork(string $name = "default", array $config = [])
+ * @method static \EasyWeChat\MicroMerchant\Application  microMerchant(string $name = "default", array $config = [])
+ */
+class EasyWechat
+{
+    public static function __callStatic($functionName, $args)
+    {
+        return ApplicationContext::getContainer()->get(Factory::class)->{$functionName}(...$args);
+    }
+}
